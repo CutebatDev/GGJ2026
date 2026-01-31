f7ec4b956829a19e0ed7da172d63d4f1bf74d9fc
CutebatDev <alexaverten@gmail.com>
2026-01-31 12:48:08 +0200

    Level switcher, minor fixes

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -1,11 +1,54 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class GameStateManager : MonoBehaviour
 {
+    [SerializeField] private InputActionReference NextLevelInput;
+    [SerializeField] private InputActionReference RestartLevelInput;
+
+    static private int currentLevelNumber = 0;
+
+    private void OnEnable()
+    {
+        NextLevelInput.action.started += LoadNextLevelPress;
+        RestartLevelInput.action.started += RestartScenePress;
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+
+    private static void RestartScenePress(InputAction.CallbackContext ctx)
+    {
+        RestartScene();
+    }
     public static void RestartScene()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        LoadLevel(currentLevelNumber);
+    }
+
+    public static void LoadScene(Scene newScene)
+    {
+        SceneManager.LoadScene(newScene.name);
+    }
+    
+    public static void LoadLevel(int levelNumber)
+    {
+        currentLevelNumber = levelNumber;
+        SceneManager.LoadScene(levelNumber);
+    }
+
+    private static void LoadNextLevelPress(InputAction.CallbackContext ctx)
+    {
+        LoadNextLevel();
+    }
+    public static void LoadNextLevel()
+    {
+        LoadLevel(currentLevelNumber + 1);
     }
 
 }
