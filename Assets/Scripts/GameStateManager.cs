eb256b2c89c75f1f2191b567e2d909965187edcd
Tamir Yamaleyev <tamirka8@gmail.com>
2026-01-31 13:20:10 +0200

    Simple MainMenu

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -1,7 +1,20 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameStateManager : MonoBehaviour
 {
-    public void RestartScene() { }
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 
+    public void StartGame(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
 }
